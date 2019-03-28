9609795c71e47c36728f94f51c3be16bcae65435
Tobias Deißler <tobias_christian.deissler@student.reutlingen-university.de>
2019-03-29 00:28:40 +0100

    Adaptions for heroku deployment.

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const WordFrequency = require('./word_frequency');
 const Dictionary    = require('./dictionary');
 const Dexonline     = require('./dexonline');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 
 WordFrequency.loadWordFrequencyTable();
